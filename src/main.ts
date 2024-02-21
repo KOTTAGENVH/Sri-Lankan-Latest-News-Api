e8bfa7cf58e29ee22e92ad5b685a9c2ddf6fb950
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-02-21 19:03:34 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
+  try {
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Server is running on http://localhost:${port}`);
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
 }
+
 bootstrap();
