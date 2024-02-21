5613875722aac288fd77837af08ec31e3a0e055f
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-02-21 19:05:06 +0530

    u

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    const port = process.env.PORT || 3000;
+    const port = 3000;
     await app.listen(port);
     console.log(`Server is running on http://localhost:${port}`);
   } catch (error) {
