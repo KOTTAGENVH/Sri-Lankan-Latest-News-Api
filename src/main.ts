d88ec0cd90c55dcb170fd204262b735d3b7ac117
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-02-21 18:55:59 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
 }
 bootstrap();
