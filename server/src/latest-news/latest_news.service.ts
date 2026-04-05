ed1d7b7076245b72d41d82b81e1d3b472673370a
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2026-04-05 11:14:05 +0530

    fix: bot issue

--- server/src/latest-news/latest_news.service.ts
+++ server/src/latest-news/latest_news.service.ts
@@ -23,7 +23,15 @@ export class LatestNewsService {
     const id = setTimeout(() => controller.abort(), timeout);
 
     try {
-      const response = await fetch(url, { signal: controller.signal });
+      const response = await fetch(url, { 
+        signal: controller.signal,
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+          'Accept-Language': 'en-US,en;q=0.9,si;q=0.8',
+        }
+      });
+
       if (!response.ok) {
         throw new Error(`HTTP Error: ${response.status}`);
       }
